b15d0d9c8243530193cdd58e475cb4b798282ab3
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-10-09 20:26:10 -0400

    Complete card functionality tests

---
+++ src/card.js
@@ -0,0 +1,11 @@
+createCard = (id, question, answers, correctAnswer) => {
+  var card = {
+    id: id,
+    question: question,
+    answers: answers,
+    correctAnswer: correctAnswer
+  }
+  return card;
+}
+
+module.exports = {createCard};
